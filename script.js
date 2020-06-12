2fb14574be281068076f30218f90ce1dd200e0e6
Rai <raikcuerda19@gmail.com>
2020-06-11 19:44:11 -0500

    add moles

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function run(){
 
 let buton = document.querySelector(".timer");
 buton.addEventListener("click",() => run())
+
+const moles = [
+    {
+        id : 1,
+        lives : 1,
+        type : "newbie",
+        stade : "hided",
+        level : "easy"
+    },
+    {
+        id : 2,
+        lives : 2,
+        type : "mole",
+        stade : "hided",
+        level : "regular"
+    },
+    {
+        id : 3,
+        lives : 3,
+        type : "moster mole",
+        stade : "hided",
+        level : "hard"
+    }
+]
