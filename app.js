c19fed11e05d18e0bd22ff90767334cb8307c489
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-06-12 22:41:11 -0500

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 const lg = console.log.bind(console);
 const buton = document.querySelector(".timer");
+const stat = document.querySelector('.stats');
+const molesLives = {};
 const App = {};
 let time = 10;
 
@@ -57,7 +59,11 @@ function getRandom(min, max) {
 function excludeAndGetRandom(array, excludedItem) {
   const filteredArray = array.filter((item) => item !== excludedItem);
   const randomIndex = Math.floor(Math.random() * filteredArray.length);
-  return filteredArray[randomIndex];
+  if (filteredArray[randomIndex] === undefined) {
+    return excludedItem;
+  } else {
+    return filteredArray[randomIndex];
+  }
 }
 
 function getRandom(array) {
@@ -68,7 +74,7 @@ function getRandom(array) {
 function initGame() {
   App.game = document.querySelector('.game');
   App.holes = [0, 0, 0].map(() => createHole());
-  App.moles = ['regular', 'monster'].map((type) => createMole(type));
+  App.moles = ['regular', 'newbie'].map((type) => createMole(type));
   App.game.append(...App.holes);
 }
 
@@ -82,78 +88,115 @@ function startTime() {
 
 function playMole() {
   App.activeHole = excludeAndGetRandom(App.holes, App.activeHole);
-  App.activeMole = getRandom(App.moles.filter((mole) => mole.dataset.lives > 0));
-  if (App.activeMole === undefined) return;
+  App.activeMole = excludeAndGetRandom(
+    App.moles.filter((mole) => mole.dataset.lives > 0),
+    App.activeMole,
+  );
+  if (App.activeMole.dataset.lives === 0) return;
   App.activeHole.append(App.activeMole);
 
   setTimeout(() => App.activeMole.up(), 0);
 
   App.activeMole.addEventListener('click', function clickEvent() {
     App.activeMole.dataset.lives -= 1;
-    lg(App.activeMole.dataset.lives);
-    App.activeMole.down();
     App.activeMole.removeEventListener('click', clickEvent);
+  });
+
+  App.activeMole.addEventListener('transitionend', function transitionEvent() {
+    App.activeMole.down();
+    App.activeMole.removeEventListener('transitionend', transitionEvent);
     if (time > 0) playMole();
   });
 
-  // App.activeMole.addEventListener('transitionend', function transitionEvent() {
-  //   lg(App.activeMole);
+  // App.activeMole.addEventListener('click', function clickEvent() {
+  //   App.activeMole.dataset.lives -= 1;
+  //   lg(App.activeMole.dataset.lives);
   //   App.activeMole.down();
-  //   App.activeMole.removeEventListener('transitionend', transitionEvent);
+  //   App.activeMole.removeEventListener('click', clickEvent);
   //   if (time > 0) playMole();
   // });
 }
 
-
-/*
-function linkMoleToHole() {
-  App.activeHole = App.holes[getRandom(0, App.holes.length)];
-  App.activeMole = App.moles[getRandom(0, App.moles.length)];
-
-  App.activeHole.append(App.activeMole);
-  setTimeout(() => App.activeMole.up(), 0);
-
-  App.activeMole.addEventListener('click', function clickEvent() {
-    App.activeMole.removeEventListener('click', clickEvent);
-    App.activeMole.down();
-
-    setTimeout(() => {
-      App.activeHole.removeChild(App.activeMole);
-      linkMoleToHole();
-    }, App.activeMole.dataset.transitionTime * 1000);
-  });
-
-  // setTimeout(() => {
-  //   App.activeMole.down();
-  // }, App.activeMole.dataset.transitionTime * 1000);
+/******************** Counter lives *******************/
+function createStats(){
+  const stats = document.createElement('DIV');
+  stats.classList.add('type-stat');
+  return stats;
 }
-*/
 
-// function startGame(){
-//   for(let i=0; i<5; i++){
-//     linkMoleToHole();
-//     showMole();
-//     setTimeout(hidedMole,500);
-//   }
-// }
+function createTypeName(name){
+  const statName = document.createElement('P');
+  statName.classList.add('type-name');
+  statName.textContent = name+ " Moles:";
+  return statName;
+}
+
+function createMoleWrapper(){
+  const moleWrapper = document.createElement('DIV');
+  moleWrapper.classList.add('mole-stat-wrapper');
+  return moleWrapper;
+}
+
+function createMoleStat(){
+  const moleStat = document.createElement('DIV');
+  moleStat.classList.add('mole-stat');
+  return moleStat;
+}
+
+function createLives(){
+  const moleLive = document.createElement('DIV');
+  moleLive.classList.add('lives');
+  return moleLive;
+}
+
+function counterLives() {
+  molesLives.easy = App.moles.filter((mole) => (
+    mole.dataset.type === 'newbie'
+  )).map((mole) => mole.dataset.lives);
+
+  molesLives.mediun = App.moles.filter((mole) => (
+    mole.dataset.type === 'regular'
+  )).map((mole) => mole.dataset.lives);
+
+  molesLives.hard = App.moles.filter((mole) => (
+    mole.dataset.type === 'monster'
+  )).map((mole) => mole.dataset.lives);
+}
+
+function renderStats() {
+  stat.innerHTML = '';
+  for (let level in molesLives) {
+    if (molesLives[level].length > 0) {
+      const newStat = createStats();
+      const newStatName = createTypeName(level);
+      const molesWeapper = [];
+      molesLives[level].forEach((live)=>{
+        const newWeapper = createMoleWrapper();
+        const newMoleStat = createMoleStat();
+        let lives = [];
+        for(let i=0; i<Number(live); i++){
+          lives.push(createLives());
+        }
+        newWeapper.append(newMoleStat,...lives);
+        molesWeapper.push(newWeapper);
+      })
+      newStat.append(newStatName,...molesWeapper);
+      stat.append(newStat);
+    }
+  }
+}
+
+function callRender() {
+  const intervalId = setInterval(() => {
+    counterLives();
+    renderStats();
+    if (time === 0) clearInterval(intervalId);
+  }, 250);
+}
 
 function run(){
-  initGame(); // render the holes y create the moles
-  startTime(); // inicializamos contador
+  initGame();
+  startTime();
   playMole();
-  // startGame();
+  callRender();
 }
-
-// En la console mostrar el topo activo
-//App.activeMole
-// Subimos al topo
-//App.activeMole.up()
-// Bajamos al topo
-//App.activeMole.down()
-
-// Ciclo Topo
-// selectMole();
-// insertInHole();
-// mole.up();
-// mole.down();
-// selectMole();
